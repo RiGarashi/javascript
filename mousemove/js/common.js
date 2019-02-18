3200db493746b57827992634ade1fa76bf8a5a3c
Risa <43200260+RiGarashi@users.noreply.github.com>
2019-02-18 11:12:06 +0900

    Add files via upload

---
+++ mousemove/js/common.js
@@ -0,0 +1,12 @@
+$(window).load(function() {
+    $(".twentytwenty-container").twentytwenty({
+        default_offset_pct: 0.8,
+        orientation: 'horizontal',
+        before_label: 'Before',
+        after_label: 'After',
+        no_overlay: true,
+        move_slider_on_hover: true,
+        move_with_handle_only: false,
+        click_to_move: false
+});
+});
